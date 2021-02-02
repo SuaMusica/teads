34181e9df6d3c47546332db2128c953f1b9f7152
Alan Trope <atrope@gmail.com>
2021-02-02 17:22:45 -0300

    adding ios code

--- ios/Classes/SwiftTeadsPlugin.swift
+++ ios/Classes/SwiftTeadsPlugin.swift
@@ -5,6 +5,11 @@ public class SwiftTeadsPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "teads", binaryMessenger: registrar.messenger())
     let instance = SwiftTeadsPlugin()
+    let factory = TeadsViewFactory(messenger: registrar.messenger())
+    registrar.register(
+                factory,
+                withId: "TeadsPlatformView")
+
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
